3686747cce5fdbde63da2b740ea92353d8eeceb9
kavyahegade <kavyashreeubdt@gmail.com>
2020-11-09 16:31:36 +0530

    Second commit

---
+++ GitProjects/src/main/java/com/kh/GitProjectsApplication.java
@@ -0,0 +1,22 @@
+package com.kh;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class GitProjectsApplication {
+	
+	
+	@GetMapping("/some")
+	public String getdata() {
+		return "get some data";
+	}
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitProjectsApplication.class, args);
+	}
+
+}
